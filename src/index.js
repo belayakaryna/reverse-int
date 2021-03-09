24dd807f70779578f4060309f064904a48a30d93
belayakaryna <belaya.karina1998@gmail.com>
2021-03-09 20:30:01 +0300

    init:add code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+ array=String(n).split("")
+    if(array[0]=="-"){
+      array.splice(0,1)
+      return(Number(array.reverse().join("")))
+        }
+    else{
+      return(Number(array.reverse().join("")))
+    }
 }
